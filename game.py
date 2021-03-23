ea0eb724586c7835d00848650f0c5916b991f69c
Yuseon <itsyuseon@gmail.com>
2021-03-23 15:35:55 +0900

    test

---
+++ game.py
@@ -0,0 +1,21 @@
+print("Starting a Game")
+print("시작")
+print("1. 오른쪽 2. 왼쪽 3. 블록 바꾸기")
+number = input("숫자 입력하시오")
+print("입력한 숫자는 ",number)
+
+if int(number)==1:
+    print("Right")
+elif number=='2':
+    print("Left")
+elif number=='3':
+    print("Fucking Changed")
+else:
+    print("get the right number between 1 to 3 idiot!!")
+
+
+
+# 1번을 누르면 오른쪽 이동
+# 2번을 누르면 왼쪽 이동
+# 3번을 누르면 블록 바꾸기
+
